759b4605898cc23f690dadf1f10dea62c9f25ab5
Walmyr <wlsf82@gmail.com>
2021-03-09 19:04:06 +0100

    Add test for "Loading ..." state

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -331,3 +331,21 @@ context('Errors', () => {
       .should('be.visible')
   })
 })
+
+it('shows a "Loading ..." state before showing the results', () => {
+  cy.intercept(
+    'GET',
+    '**/search**',
+    {
+      delay: 1000,
+      fixture: 'stories'
+    }
+  ).as('getDelayedStories')
+
+  cy.visit('/')
+
+  cy.assertLoadingIsShownAndHidden()
+  cy.wait('@getDelayedStories')
+
+  cy.get('.item').should('have.length', 2)
+})
